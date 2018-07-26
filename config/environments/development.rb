0456ba7e530be4c9782d2dbf1a773b5ad1108148
abdelmaseeh <abdelmaseeh.hanna@gmail.com>
2018-07-26 15:07:56 +0200

    Add account activation (not yet in production)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,6 +49,12 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
 
+  # configuration for mailing system
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = 'localhost:3000'                     # Local server
+  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
